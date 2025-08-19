4ef4c3f25178a5317afc95fa221c9820b3505f06
shammah <shamaj2006@gmail.com>
2025-08-19 12:19:29 +0100

    dom

--- static/scripts/09-dom.js
+++ static/scripts/09-dom.js
@@ -1,2 +1,12 @@
-document.body.innerHTML = 'hello';
-document.title = 'fack'
+console.log(document.querySelector('button').innerHtml);
+document.querySelector('button').innerHtml = 'changed';
+
+// console.log(document.title);
+// document.title = 'changed';
+// console.log(document.body);
+// console.log(document.body.innerHTML);
+// document.body.innerHTML = '<button>First button</button>';
+
+
+// document.body.innerHTML = 'hello';
+// document.title = 'fack'
