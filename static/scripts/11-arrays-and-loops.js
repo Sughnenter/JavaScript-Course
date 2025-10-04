3baac1d55036ffe48f516b6a56d9061cdf74bb55
shammah <shamaj2006@gmail.com>
2025-10-04 11:15:35 +0100

    arrays and loops

---
+++ static/scripts/11-arrays-and-loops.js
@@ -0,0 +1,14 @@
+const myArray = [10, 20, 30, 40];
+console.log(myArray);
+myArray[0] = 100;
+console.log(myArray);
+
+[1, 'hello', true, { name: 'socks' }[1, 2, 3]];
+console.log(typeof [1, 2]);
+console.log(Array.isArray([1, 2]));
+
+console.log(myArray.length);
+myArray.push(50);
+console.log(myArray);
+myArray.splice(0, 1);
+console.log(myArray);
