24bb4d546924ab41e62d31a8b2877ef10a69111c
shammah <shamaj2006@gmail.com>
2025-10-06 15:33:30 +0100

    display todo list on webpage

--- static/scripts/11-to-do-list.js
+++ static/scripts/11-to-do-list.js
@@ -1,9 +1,30 @@
+
+const todoList = ['make dinner', 'wash clothes'];
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML)
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML
+}
+
+
 const firstInput = document.querySelector('.first-todo-input');
-const todoList = [];
-
-
 function addTodo() {
   const todoName = firstInput.value;
   todoList.push(todoName);
   console.log(todoList);
+  renderTodoList()
+}
+const secondInput = document.querySelector('.second-todo-input');
+function addsecondTodo() {
+  const todoName = secondInput.value;
+  todoList.push(todoName);
+  console.log(todoList);
+  renderTodoList()
 }
