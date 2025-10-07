3c174ae666958a489f3589e77df5e5b69a51fd91
shammah <shamaj2006@gmail.com>
2025-10-07 16:02:08 +0100

    changed for loop to for each

--- static/scripts/12-advanced-functions.js
+++ static/scripts/12-advanced-functions.js
@@ -1,27 +1,36 @@
-function greeting() {
-    console.log("Hello World");
-}
-greeting();
-const num = 2;
-const function1 = function greeting() {
-    console.log("Hello World2");
-};
+// function greeting() {
+//     console.log("Hello World");
+// }
+// greeting();
+// const num = 2;
+// const function1 = function greeting() {
+//     console.log("Hello World2");
+// };
 
-console.log(function1); //prints the function definition
-console.log(typeof function1); //prints function type
-function1(); //calls the function
-
-
-const object1 = { num: 2, fun: function greeting() { console.log("Hello World3") } };
-object1.fun(); //calls the function inside the object
-
-setTimeout(function () {
-    console.log("Timeout");
-    console.log("Timeout2");
-}, 3000); //calls the function after 3 seconds the second parrameter is the time in milliseconds
-console.log("Next line");
+// console.log(function1); //prints the function definition
+// console.log(typeof function1); //prints function type
+// function1(); //calls the function
 
 
-setInterval(function () {
-    console.log("Interval");
-}, 3000); //calls the function every 3 seconds the second parrameter is the time in milliseconds
+// const object1 = { num: 2, fun: function greeting() { console.log("Hello World3") } };
+// object1.fun(); //calls the function inside the object
+
+// setTimeout(function () {
+//     console.log("Timeout");
+//     console.log("Timeout2");
+// }, 3000); //calls the function after 3 seconds the second parrameter is the time in milliseconds
+// console.log("Next line");
+
+
+// setInterval(function () {
+//     console.log("Interval");
+// }, 3000); //calls the function every 3 seconds the second parrameter is the time in milliseconds
+
+[
+    'make dinner',
+    'wash dishes',
+    'take out trash',
+].forEach(function (value, index) {
+    console.log(index);
+    console.log(value);
+})
---
+++ static/scripts/12-to-do-list.js
@@ -0,0 +1,40 @@
+
+const todoList = [{ name: 'make dinner', dueDate: '2025-10-07' }, { name: 'wash clothes', dueDate: '2025-10-07' }];
+function renderTodoList() {
+  let todoListHTML = '';
+
+  todoList.forEach(function (todoObject, index) {
+    const { name, dueDate } = todoObject; //destructuring, does the same thing as the line of code above
+    const html = `<div> ${name}</div> <div> ${dueDate}</div>
+    <button class="delete-button" onclick="
+    todoList.splice(${index}, 1); renderTodoList()"
+    >Delete</button> `;
+    todoListHTML += html;
+  }
+  );
+
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML
+}
+
+
+const firstInput = document.querySelector('.first-todo-input');
+function addTodo() {
+  const todoName = firstInput.value;
+  todoList.push(todoName);
+  renderTodoList()
+}
+const secondInput = document.querySelector('.second-todo-input');
+function addsecondTodo() {
+  const todoName = secondInput.value;
+  todoList.push(todoName);
+  renderTodoList()
+}
+const dateInput = document.querySelector('.date-todo-input')
+const FinalInput = document.querySelector('.todo-input');
+function addFinalTodo() {
+  const name = FinalInput.value;
+  const dueDate = dateInput.value;
+  todoList.push({ name, dueDate });
+  renderTodoList()
+}
